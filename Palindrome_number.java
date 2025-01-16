6d0573dcb5a9bec8d2e3ae85af7e09977bcaa5f0
LexusLXR <68087923+LexusLXR@users.noreply.github.com>
2025-01-16 14:12:35 +0530

    another update

---
+++ Palindrome_number.java
@@ -0,0 +1,21 @@
+public class Palindrome_number {
+    class Solution {
+        public boolean isPalindrome(int x) {
+            if(x<0){
+                return false;
+            }else if(x==0){
+                return true;
+            }
+    
+            long ans = 0;
+            int org = x;
+            while(x!=0){
+                int rem = x%10;
+                x = x/10;
+                ans = ans*10 + rem;
+            }
+            return ans == org;
+        }
+    }
+    
+}
---
+++ typecast.java
@@ -0,0 +1,9 @@
+public class typecast {
+    public  static void main(String []args){
+        String a = "1234567";
+        
+        int number = Integer.parseInt(a);
+        System.out.println(number);
+    }
+    
+}
